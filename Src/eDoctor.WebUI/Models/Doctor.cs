73345cdf71524c77f6e35523971a95852acb4f42
Dhananjeyan Balaretnaraja <sdbala_81@yahoo.com>
2011-06-13 17:57:20 +0530

    added Doctor Model class

---
+++ Src/eDoctor.WebUI/Models/Doctor.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace eDoctor.WebUI.Models
+{
+    public class Doctor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime DoB { get; set; }
+        public string Mobile { get; set; }
+        public string LandLine { get; set; }
+        public string Qualification { get; set; }
+    }
+}
